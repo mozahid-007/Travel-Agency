3648d21913b6f04e3ac7bcacc95cc8ecba937639
Md Mozahid <99892827+mozahid-007@users.noreply.github.com>
2024-01-01 15:44:10 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let menu = document.querySelector('#menuBtn');
+let navbar = document.querySelector('.header .sideNav');
+
+menu.onclick = () =>{
+
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
